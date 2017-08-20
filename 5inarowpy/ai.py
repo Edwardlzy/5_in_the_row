ca454b083a51f6ebd45a3f89a6da3dc0b749f778
sandinbowl <luyao.yang@mail.utoronto.ca>
2017-08-20 18:31:03 +0100

    nothing to say

---
+++ 5inarowpy/ai.py
@@ -0,0 +1,9 @@
+class ai:
+
+
+
+    def next_move(self, state):
+        pass
+
+    def decide(self):
+        pass
--- 5inarowpy/playing.py
+++ 5inarowpy/playing.py
@@ -1,6 +1,7 @@
 
 
 from state import *
+from ai import *
 
 computer = 0
 human = 1
@@ -20,8 +21,8 @@ purple = 9
 class play:
 
 
-    def __init__(self, board_size = 11, human_colour = black, computer_colour = white):
-        self.board_size = board_size
+    def __init__(self, board_size = 15, human_colour = black, computer_colour = white):
+        self.board_size = board_size # only accept odd size to enable a centre.
         self.human_colour = human_colour
         self.computer_colour = computer_colour
         self.curr_state = None
@@ -31,20 +32,14 @@ class play:
         self.curr_state = State(self.board_size, mapR, mapI)
         if who_first == human:
             # wait for a listener to give us a place.
-            place = (0,0)
-            self.human_first_play(place)
+            place = (0,0) # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            self.curr_state.new_step(place, human)
             next = computer
         else:
-            self.computer_first_play()
+            centre = self.board_size // 2
+            self.curr_state.new_step((centre, centre), computer)
             next = human
         self.gaming(next)
-
-    def computer_first_play(self):
-        centre = (self.board_size + 1) // 2 - 1
-        self.curr_state.first_step((centre, centre), computer)
-
-    def human_first_play(self, place):
-        self.curr_state.first_step(place, human)
 
     def usual_play(self, place, who):
         self.curr_state.new_step(play, who)
@@ -52,11 +47,10 @@ class play:
     def gaming(self, next): # a recursive call to keep the game move on.
         if next == human:
             # listener tell the place.
-            place = (0, 0)
+            place = (0, 0) # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
             self.usual_play(place, human)
-            ai.human_move(self.curr_state)
             next = computer
         else:
-            move = ai.next_move()
+            move = ai.next_move(self.curr_state)
             next = human
         self.gaming(next)
